050017810bedb70b880821ae6e462fee0a22c923
yemisrach-19 <yemi.t.maki@gmail.com>
2025-05-29 03:05:19 -0800

    Category and Product

--- amazon/src/App.jsx
+++ amazon/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Header from './Components/Header/Header'
 import  Carousel  from './Components/Carousel/Carousel'
+import Category from './Components/Category/Category'
+import Product from './Components/product/Product'
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
 
       <Header/>
       <Carousel/>
+      <Category/>
+      <Product/>
     </div>
   )
 }
